d2ec7b0c31e9bc9193fde8ab9cd9d4cab559e090
Chris Hellmuth <chellmuth@gmail.com>
2009-03-27 22:34:21 -0700

    First functional card row parts.
    * Refreshing rotates cards across the card row until they run out.
    * All the Age A cards.

--- tta/game/models.py
+++ tta/game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    file_id = models.CharField(max_length=40)
+
+class Deck(models.Model):
+    card = models.ForeignKey(Card)
+    rank = models.IntegerField()
+
+class Board(models.Model):
+    card = models.ForeignKey(Card)
+    spot = models.IntegerField()
--- tta/game/views.py
+++ tta/game/views.py
@@ -1,4 +1,52 @@
 from django.shortcuts import render_to_response
+from game.models import Board, Deck
+
 
 def index(request):
-    return render_to_response('game/index.html')
+    layout = get_layout()
+    fill_board(layout)
+
+    card_row = [ card.card.file_id for card in Board.objects.all() ]
+    for index in range(len(card_row), 13):
+        card_row.append("Blank.png")
+
+    return render_to_response('game/index.html', {'card_row': card_row})
+
+
+def get_layout():
+    board = Board.objects.all()
+    card_hash = {}
+    for card in board:
+        card_hash[card.spot] = card
+
+    layout = [ None for _ in range(13) ]
+    print card_hash.keys()
+    for index in card_hash.keys():
+        layout[index] = card_hash[index]
+
+    return layout
+
+def fill_board(layout):
+    first = layout[0]
+    if first:
+        first.delete()
+    layout[0] = None
+    layout = [ x for x in layout if x ]
+    for index in range(len(layout), 13):
+        layout.append(new_card(index))
+
+    for index, card in enumerate(layout):
+        if not card: continue
+        card.spot = index
+        card.save()
+
+def new_card(index):
+    deck = list(Deck.objects.all().order_by('rank'))
+    if not deck: return None
+
+    card = deck[0]
+    card_obj = card.card
+    card.delete()
+    board = Board(card=card_obj, spot=index)
+    board.save()
+    return board
